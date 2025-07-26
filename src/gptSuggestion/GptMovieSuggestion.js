d396311a590b8c30662eee2e57fb99d979a2ccae
Pritam Lamkhade <lamkhadepritam2905@gmail.com>
2025-07-26 22:08:09 +0530

    Language support for GPT movie suggestion fallback message

--- src/gptSuggestion/GptMovieSuggestion.js
+++ src/gptSuggestion/GptMovieSuggestion.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import MovieList from "../movies/MovieList";
+import { useTranslation } from "react-i18next";
 
 const GptMovieSuggestion = () => {
   const { gptMovieNames, gptMovies } = useSelector((store) => store.gpt);
+  const { t } = useTranslation();
 
   if (gptMovieNames && gptMovieNames.length > 0) {
     return (
@@ -18,11 +20,9 @@ const GptMovieSuggestion = () => {
   return (
     <div className="text-center mt-20">
       <h2 className=" text-gray-400 text-2xl font-bold mt-6">
-        Find the perfect movie 🎬
+        {t("gpt_no_results_heading")}
       </h2>
-      <p className="text-gray-400 text-lg">
-        Ask GPTto suggest movies by genre, mood, or even your favorite film!
-      </p>
+      <p className="text-gray-400 text-lg">{t("gpt_no_results_description")}</p>
     </div>
   );
 };
