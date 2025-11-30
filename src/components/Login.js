fd4ed286faa268a1cca40095679cf0f6fbf34875
Pritam Lamkhade <lamkhadepritam2905@gmail.com>
2025-11-30 16:28:08 +0530

    minor changes

--- src/components/Login.js
+++ src/components/Login.js
@@ -32,14 +32,14 @@ const Login = () => {
       const user = await loginUser(email.current.value, password.current.value);
       if (user) {
         const { uid, email, displayName, photoURL } = auth.currentUser;
-        dispatch( {type:addUser, payload:
-          {
+       dispatch(
+          addUser({
             uid: uid,
             email: email,
             displayName: displayName,
             photoURL: photoURL,
-          }}
-        )
+          })
+        );
       }
     } catch (error) {
       setErrorMessage(t("Invalid email or password."));
